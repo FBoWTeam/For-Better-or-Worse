c4f3df0be179dfaeb2f75f5138afd26d0d3bae26
Nathan Desages <nathan.desages@gmail.com>
2019-01-16 12:33:53 +0100

    ajout de l'orbe et préparation du script de l'orbe

---
+++ Assets/Scripts/OrbController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbController : MonoBehaviour
+{
+	public LineRenderer link;
+
+	public float speed;
+
+	bool ascending;
+	int start, end;
+	float step;
+	
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+        
+    }
+}
