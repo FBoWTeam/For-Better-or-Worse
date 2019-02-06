ea07e44316314239fddbefa5251c4a1bf77c4da9
Piteul <axel.tetart@gmail.com>
2019-02-06 15:41:33 +0100

    Create personal Scene & enemy prefab

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Enemy : MonoBehaviour
+{
+    public enum Movement { Immobile, Classic };
+    public Movement movement;
+
+    public enum Skill { Impact, Distance };
+    public Skill skillOne;
+    public Skill skillTwo;
+
+    public enum Bonus { Mirror}
+    public Bonus bonusOne;
+    public Bonus bonusTwo;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
