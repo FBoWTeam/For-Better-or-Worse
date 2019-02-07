d955ddb2a2832125b8cf43d2948a067ec896117c
Nathan Desages <nathan.desages@gmail.com>
2019-02-07 13:57:12 +0100

    add orbcontroller2

--- Assets/Scripts/BezierCurve.cs
+++ Assets/Scripts/BezierCurve.cs
@@ -7,14 +7,22 @@ public class BezierCurve : MonoBehaviour
 {
 	LineRenderer lineRenderer;
 
-	public Transform player1, player2;
-	Transform deformP1;
-	Transform deformP2;
+	static Transform player1, player2;
+	static Transform deformPoint1, deformPointMid, deformPoint2;
 
 	static private int numberPoints = 20;
 	private Vector3[] positions = new Vector3[numberPoints];
 
 	void Awake()
+	{
+		player1 = GameObject.Find("Player1").transform;
+		player2 = GameObject.Find("Player2").transform;
+		deformPoint1 = transform.GetChild(0).GetChild(0);
+		deformPointMid = transform.GetChild(0).GetChild(1);
+		deformPoint2 = transform.GetChild(0).GetChild(2);
+	}
+
+	void Start()
 	{
 		lineRenderer = GetComponent<LineRenderer>();
 		lineRenderer.positionCount = numberPoints;
@@ -28,19 +36,37 @@ public class BezierCurve : MonoBehaviour
 		transform.position = player1.position + (player2.position - player1.position) / 2;
 	}
 
+	/// <summary>
+	///	Set the positions of the points in the LineRenderer
+	/// </summary>
 	private void DrawCubicCurve()
 	{
 		for (int i = 0; i < positions.Length; i++)
 		{
-			float t = i / (float)(numberPoints-1);
-			positions[i] = CalculateCubicBezierPoint(t, player1.position, deformP1.position, deformP2.position, player2.position);
+			float t = i / (float)(numberPoints - 1);
+			positions[i] = CalculateCubicBezierPoint(t);
 		}
 		lineRenderer.SetPositions(positions);
 	}
 
-	private Vector3 CalculateCubicBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+	/// <summary>
+	///	Calculate a point on the cubic bezier curve based on the param "t" between 0 (begining of the curve) and 1 (end of the curve)
+	/// </summary>
+	/// <param name="t"></param>
+	/// <returns></returns>
+	public static Vector3 CalculateCubicBezierPoint(float t)
 	{
-		return Mathf.Pow((1 - t), 3) * p0 + 3 * Mathf.Pow((1 - t), 2) * t * p1 + 3 * (1 - t) * t * t * p2 + Mathf.Pow(t, 3) * p3;
+		//return Mathf.Pow((1 - t), 3) * player1.position + 3 * Mathf.Pow((1 - t), 2) * t * deformPoint1.position + 3 * (1 - t) * t * t * deformPoint2.position + Mathf.Pow(t, 3) * player2.position;
+		return Mathf.Pow((1 - t), 4) * player1.position + 4 * t * Mathf.Pow((1 - t), 3) * deformPoint1.position + 6 * Mathf.Pow(t, 2) * Mathf.Pow((1 - t), 2) * deformPointMid.position + 4 * Mathf.Pow(t, 3) * (1 - t) * deformPoint2.position + Mathf.Pow(t, 4) * player2.position;
+	}
+
+	/// <summary>
+	///	return the distance between player 1 and 2
+	/// </summary>
+	/// <returns></returns>
+	public static float GetPlayersDistance()
+	{
+		return Vector3.Distance(player1.position, player2.position);
 	}
 
 }
