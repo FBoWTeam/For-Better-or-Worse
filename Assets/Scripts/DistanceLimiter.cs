55f6b092f283f3d4d03c2a759dbab7083eab0a6f
valentinmoulard <moulardvalentin@gmail.com>
2019-02-07 13:50:53 +0100

    Update player controller et commentaires

--- Assets/Scripts/DistanceLimiter.cs
+++ Assets/Scripts/DistanceLimiter.cs
@@ -12,6 +12,13 @@ public class DistanceLimiter : MonoBehaviour
 
     private BoxCollider[] boxList;
 
+    /// <summary>
+    /// Script qui gere les distances Min et Max entre les joueurs
+    /// Géré avec des colliders positionné en fonction de la position des joueurs
+    /// Taille des colliders parametrable
+    /// Distance Min gérée avec une sphere colliders situé entre les joueurs
+    /// Distance Max gérée avec deux box colliders situés deriere chaque joueurs
+    /// </summary>
     private void Start()
     {
         boxList = gameObject.GetComponents<BoxCollider>();
@@ -34,5 +41,4 @@ public class DistanceLimiter : MonoBehaviour
         boxList[1].center = new Vector3(0.0f, 0.0f, -maxDistance / 2);
         gameObject.GetComponent<SphereCollider>().radius = minDistance / 2;
     }
-
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,33 +5,37 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
-	public bool player1;
-	public float speed;
+    public bool player1;
+    public float speed;
 
     public float dashTime;
     public float dashSpeed;
 
     bool isDashing = false;
-	Rigidbody rb;
+    Rigidbody rb;
     Vector3 direction;
 
     void Awake()
-	{
-		rb = GetComponent<Rigidbody>();
-	}
-	
-	void Update()
-	{
-		Move();
-	}
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
+    void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// Gere les déplacements du joueurs (clavier et manette) + gestion du dash
+    /// Player 1 : ZQSD + Space ; Player2 : OKLM + I
+    /// </summary>
 	public void Move()
-	{
+    {
         if (!isDashing)
         {
             if (player1)
             {
-               direction = new Vector3(Input.GetAxis("HorizontalP1"), 0.0f, Input.GetAxis("VerticalP1"));
+                direction = new Vector3(Input.GetAxis("HorizontalP1"), 0.0f, Input.GetAxis("VerticalP1"));
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     StartCoroutine(DashCoroutine());
@@ -49,7 +53,8 @@ public class PlayerController : MonoBehaviour
 
         Vector3 velocity = direction * speed * Time.deltaTime;
         rb.MovePosition(transform.position + velocity);
-	}
+    }
+
 
     IEnumerator DashCoroutine()
     {
@@ -59,6 +64,4 @@ public class PlayerController : MonoBehaviour
         speed /= dashSpeed;
         isDashing = false;
     }
-
-
 }
