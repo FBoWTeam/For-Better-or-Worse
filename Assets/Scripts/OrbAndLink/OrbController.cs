5a9a0400fe21ee5b57b1fbbda9afc92d32b4cb33
Nathan Desages <nathan.desages@gmail.com>
2019-02-08 16:16:16 +0100

    preparation fix orb speed

--- Assets/Scripts/OrbAndLink/OrbController.cs
+++ Assets/Scripts/OrbAndLink/OrbController.cs
@@ -6,12 +6,15 @@ public class OrbController : MonoBehaviour
 {
 	public float speed;
 
+	Vector3 oldPos;
+
 	bool toPlayer1;
 	float progression;
 	float step;
 
 	void Start()
 	{
+		oldPos = transform.position;
 		toPlayer1 = true;
 		progression = 0.5f;
 		transform.position = BezierCurve.CalculateCubicBezierPoint(progression);
@@ -19,6 +22,9 @@ public class OrbController : MonoBehaviour
 
 	void FixedUpdate()
 	{
+		Debug.Log(Vector3.Distance(oldPos, transform.position));
+		oldPos = transform.position;
+
 		step = (speed / BezierCurve.GetPlayersDistance()) * Time.deltaTime;
 		progression = toPlayer1 ? progression + step : progression - step;
 		transform.position = BezierCurve.CalculateCubicBezierPoint(progression);
