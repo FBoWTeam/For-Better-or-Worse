1f07d9bfb2be1abd91de86366b8d91c7635c2d16
valentinmoulard <moulardvalentin@gmail.com>
2019-02-11 15:13:39 +0100

    base pour les object interactibles
    oui

---
+++ Assets/Scripts/Object/Brazier.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brazier : MonoBehaviour, IActivable
+{
+    //boolean indicating if the brazier is activated or not
+    public bool isActive { get; set; }
+
+    //the brazier activates an other object
+    public GameObject objectToActivate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Orb"))
+        {
+            //if the brazier is not active and the orb is on fire, set the brazier on and activates the object if not null
+            if (isActive == false && other)
+            {
+                this.Activate();
+                if (objectToActivate != null)
+                {
+                    objectToActivate.GetComponent<IActivable>().Activate();
+                }
+            }
+            //if the brazier is active and the orb isn't, set the orb on fire
+            else if (isActive == true && other)
+            {
+                //FOUT LE FEU A LA BALLE !
+            }
+        }
+    }
+
+    public void Activate()
+    {
+        isActive = !isActive;
+    }
+}
---
+++ Assets/Scripts/Object/Lever.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour, IActivable
+{
+    //boolean indicating if the lever is activated or not
+    public bool isActive { get; set; }
+
+    //the lever activates an other object
+    public GameObject objectToActivate;
+
+    /// <summary>
+    /// activates the lever if the orb enter it's collider
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Orb"))
+        {
+            this.Activate();
+        }
+    }
+
+    /// <summary>
+    /// switch the boolean when activated and activate the door or hatch
+    /// </summary>
+    public void Activate()
+    {
+        isActive = !isActive;
+        objectToActivate.GetComponent<IActivable>().Activate();
+    }
+
+
+}
