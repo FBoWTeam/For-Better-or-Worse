5fd48bd7a260a40531168e03cd57f648e3bc07e0
TouffeMystere <44296169+TouffeMystere@users.noreply.github.com>
2019-02-19 10:55:36 +0100

    repair enemy.cs

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,332 +7,98 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : MonoBehaviour
 {
-	#region All Variables
-
-	public int baseHP;
-	public int hp;
+    #region All Variables
 
     #region Debug Variables
     public bool debug;
     [DrawIf(new string[] { "debug" }, true)]
     public bool drawPath = false;
+    public bool drawView = false;
+    [DrawIf(new string[] { "drawView" }, true)]
+    public float lengthView = 2f;
+
+
+    public static bool sdrawPath;
     #endregion
 
-    #region Movement Variables
-    public enum Movement
+    public enum Focus
     {
-        Immobile,
-        Classic,
-    };
-    [Header("[Movement]")]
-    public Movement movement;
+        Player1,
+        Player2,
+        Nearest,
+    }
+    public Focus focus = Focus.Nearest;
 
-    [DrawIf(new string[] { "movement" }, Movement.Classic)]
-    public float speed = 2f;
+    public int baseHP = 100;
+    public int hp;
 
-    [DrawIf(new string[] { "movement" }, Movement.Classic)]
-    [Tooltip("represents the time of the attack animation")]
-    public float stopTime = 2f;
+    public EnemyMovement enemyMovement;
 
-    [DrawIf(new string[] { "movement" }, Movement.Classic)]
-    [Tooltip("represents the remaining distance between the enemy and the player")]
-    public float distanceBetweenPlayer = 5f;
+    GameObject[] players;
+    public static GameObject target;
 
-    private LineRenderer line;
-    #endregion
-
-    #region Skills Variables
-    public enum Skill
-    {
-        Impact,
-        AOE,
-        Distance,
-        Bloc,
-        MudThrow,
-        Vortex,
-        Inverse,
-        Mentaliste,
-        Shield,
-        PreciousWater,
-        Rooting,
-        Silence,
-        Magnet,
-        None,
-    };
-    public bool showSkill = true;
-    [Header("[Skills]")]
-    public Skill skillOne;
-    public Skill skillTwo;
-
-   
-   
-    #region ImpactFields
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.Impact)]
-    public int impactDamage = 5;
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.Impact)]
-    public float impactCooldown = 3f;
-
-    bool canAttack = true;
-    #endregion
-
-    #region AoeFields
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.AOE)]
-    public float aoeRange = 5f;
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.AOE)]
-    public int aeoDamage = 5;
-    #endregion
-
-    #region DistanceFields
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.Distance)]
-    public float distanceRange = 5f;
-
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.Distance)]
-    public int distanceDamage = 3;
-
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.Distance)]
-    public float fireRate = 1f;
-
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.Distance)]
-    public float turnSpeed = 6.5f;// Rotation Speed
-
-
-    private float fireCountdown = 0f;
-
-    //public Transform partToRotate;
-
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.Distance)]
-    public GameObject bulletPrefab;
-
-    [DrawIf(new string[] { "skillOne", "skillTwo" }, Skill.Distance)]
-    public Transform firePoint;
-    #endregion
-    #endregion
-
-    #region Bonus Variables
-    public enum Bonus
-    {
-        Mirror,
-        None,
-    };
-    [Header("[Bonus]")]
-    public Bonus bonusOne;
-    public Bonus bonusTwo;
-    #endregion
-
-
-    private bool coliding = false;
-    private NavMeshAgent agent;
-    private GameObject[] players;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        agent = this.GetComponent<NavMeshAgent>();
-        agent.speed = speed;
-        players = GameObject.FindGameObjectsWithTag("Player");
-        line = this.GetComponent<LineRenderer>();
-		hp = baseHP;
+        hp = baseHP;
+        players = new GameObject[] { GameManager.gameManager.player1, GameManager.gameManager.player2 };
+        enemyMovement = GetComponent<EnemyMovement>();
+        sdrawPath = drawPath;
     }
 
     // Update is called once per frame
     void Update()
     {
-        DoSkill(skillOne);
-        DoSkill(skillTwo);
-		
-		if(!agent.isStopped)
-		{
-			DoMovement(movement);
-		}
+        FocusManagement();
 
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 5, Color.yellow);
-		//Debug.DrawRay(transform.position, transform.forward, Color.green);
-
-		if (hp <= 0)
-		{
-			agent.isStopped = true;
-			StopAllCoroutines();
-			Destroy(this.gameObject);
-		}
-	}
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        if (!enemyMovement.agent.isStopped)
         {
-            coliding = true;
+            enemyMovement.DoMovement();
+        }
+
+        if (hp <= 0)
+        {
+            enemyMovement.agent.isStopped = true;
+            StopAllCoroutines();
+            Destroy(this.gameObject);
+        }
+
+        if (drawView)
+        {
+            Debug.DrawRay(this.transform.position, this.transform.forward * lengthView, Color.magenta);
         }
     }
 
-    #region Skill
-
-    
-
+    #region Focus Methods
     /// <summary>
-    /// Activate the skill passed in parameters
+    /// Manage which target assigned to the enemy
     /// </summary>
-    /// <param name="skill"></param>
-    void DoSkill(Skill skill)
+    public void FocusManagement()
     {
-
-        switch (skill)
+        switch (focus)
         {
-            case Skill.Impact:
-               
-                if (coliding && canAttack) {
-                    StartCoroutine("Impact");
-                }
-
+            case Focus.Player1:
+                target = GameManager.gameManager.player1;
                 break;
-            case Skill.AOE:
-               
-                AoeDamage(transform.position, aoeRange, aeoDamage);
-                
+            case Focus.Player2:
+                target = GameManager.gameManager.player2;
                 break;
-            case Skill.Distance:
-
-                GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-                //recup le plus porhce
-                GameObject nearest = GetNearestGO(players);
-                float distanceto = Vector3.Distance(transform.position, nearest.transform.position);
-
-                // if inRange && isVisible
-
-                if (nearest != null && (distanceto <= distanceRange) && isVisible(transform.position, nearest.transform.position))
-                {
-
-                    Vector3 dir = nearest.transform.position - transform.position;
-                    Quaternion lookRotation = Quaternion.LookRotation(dir);
-                    Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles; //tourner l'ennemy vers le jouer
-
-                    //Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles; // tourne le gun/arc/arme de l'ennemi 
-                    //partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f); // idem
-
-
-                    //1 Throw projectile
-                    // 2 check if projectile hits
-                    //   2.1 do domage
-
-                    if (fireCountdown <= 0f)
-                    {//active le tir si le countdown avant de tirer est inférieur ou égal à 0
-
-                        Shoot(bulletPrefab, firePoint, nearest.transform, distanceDamage);
-                        fireCountdown = 1 / fireRate;//réinitialise le countdown
-                    }
-
-                    fireCountdown -= Time.deltaTime;
-
-                }
-                break;
-            case Skill.Bloc:
-                break;
-            case Skill.MudThrow:
-                break;
-            case Skill.Vortex:
-                break;
-            case Skill.Inverse:
-                break;
-            case Skill.Mentaliste:
-                break;
-            case Skill.Shield:
-                break;
-            case Skill.PreciousWater:
-                break;
-            case Skill.Rooting:
-                break;
-            case Skill.Silence:
-                break;
-            case Skill.Magnet:
+            case Focus.Nearest:
+                target = GetNearestGO(players);
                 break;
             default:
                 break;
         }
     }
-    #endregion
-
-
-    #region Movement
-
-    void DoMovement(Movement movement)
-    {
-        switch (movement)
-        {
-            case Movement.Immobile:
-                this.transform.LookAt(players[0].transform);
-                break;
-            case Movement.Classic:
-                StartCoroutine(ClassicMovement());
-                break;
-            default:
-                break;
-        }
-
-        if (drawPath)
-        {
-            line.enabled = true;
-            DrawPath(agent.path);
-        }
-        else
-        {
-            line.enabled = false;
-        }
-    }
-
-    IEnumerator ClassicMovement()
-    {
-        agent.destination = GetNearestGO(players).transform.position;
-
-        if (agent.remainingDistance <= distanceBetweenPlayer)
-        {
-            agent.isStopped = true;
-            yield return new WaitForSeconds(stopTime);
-			agent.isStopped = false;
-		}
-    }
-
-    void DrawPath(NavMeshPath path)
-    {
-        if (path.corners.Length < 2) //if the path has 1 or no corners, there is no need
-        {
-            return;
-        }
-        line.SetPositions(path.corners); //set the array of positions to the amount of corners
-    }
-
-    #endregion
-
-    IEnumerator Impact() {
-       
-        GameManager.gameManager.takeDamage(impactDamage);
-        canAttack = false;
-        yield return new WaitForSeconds(impactCooldown);
-       
-        canAttack = true;
-    }
 
     /// <summary>
-    /// do damage to all gameObject inside a sphereCollider of center in radius
+    /// Determines the nearest Gameobject (according to position) in the list
     /// </summary>
-    /// <param name="center"></param>
-    /// <param name="radius"></param>
-    void AoeDamage(Vector3 center, float radius, int damage)
-    {
-        Collider[] hits = Physics.OverlapSphere(center, radius);
-        foreach (Collider item in hits)
-        {
-
-            if (item.CompareTag("Player"))
-            {
-                print("aeoDamage");
-                GameManager.gameManager.takeDamage(damage);
-               
-            }
-        }
-        
-    }
-
-    GameObject GetNearestGO(GameObject[] gos)
+    /// <param name="gos">Take a GameObject list</param>
+    /// <returns></returns>
+    public GameObject GetNearestGO(GameObject[] gos)
     {
         float min = Mathf.Infinity;
         GameObject nearest = null;
@@ -348,123 +114,5 @@ public class Enemy : MonoBehaviour
         return nearest;
     }
 
-    /// <summary>
-    /// Shoot a bulletPrefab from firePoint to target in order to do damage onHit
-    /// </summary>
-    /// <param name="bulletPrefab"></param>
-    /// <param name="firePoint"></param>
-    /// <param name="target"></param>
-    /// <param name="damage"></param>
-    void Shoot(GameObject bulletPrefab, Transform firePoint, Transform target, int damage)
-    {
-        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        Bullet bullet = bulletGO.GetComponent<Bullet>();//instancie un objet bullet à partir du prefab défini
-
-        if (bullet != null)
-        {
-            bullet.Seek(target, damage);
-        }
-    }
-
-    /// <summary>
-    /// return true if a Player is "visible"
-    /// </summary>
-    /// <param name="start"></param>
-    /// <param name="end"></param>
-    /// <returns></returns>
-    bool isVisible(Vector3 start, Vector3 end)
-    {
-        RaycastHit hitInfo;
-        if (Physics.Linecast(start, end, out hitInfo))
-        {
-            if (hitInfo.transform.CompareTag("Player"))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
-    private void OnDrawGizmos()
-    {
-        if (showSkill) {
-            Gizmos.color = Color.white;
-            switch (skillOne) {
-                case Skill.Impact:
-                    break;
-                case Skill.AOE:
-                    Gizmos.DrawWireSphere(transform.position, aoeRange);
-                    break;
-                case Skill.Distance:
-                    Gizmos.DrawLine(transform.position, new Vector3(-17.3f, 1.0f, 0));
-                    Gizmos.DrawWireSphere(transform.position, distanceRange);
-                    break;
-                case Skill.Bloc:
-                    break;
-                case Skill.MudThrow:
-                    break;
-                case Skill.Vortex:
-                    break;
-                case Skill.Inverse:
-                    break;
-                case Skill.Mentaliste:
-                    break;
-                case Skill.Shield:
-                    break;
-                case Skill.PreciousWater:
-                    break;
-                case Skill.Rooting:
-                    break;
-                case Skill.Silence:
-                    break;
-                case Skill.Magnet:
-                    break;
-                case Skill.None:
-                    break;
-                default:
-                    break;
-            }
-
-            Gizmos.color = Color.black;
-            switch (skillTwo) {
-                case Skill.Impact:
-                    break;
-                case Skill.AOE:
-                    Gizmos.DrawWireSphere(transform.position, aoeRange);
-                    break;
-                case Skill.Distance:
-                    Gizmos.DrawLine(transform.position, new Vector3(-17.3f, 1.0f, 0));
-                    Gizmos.DrawWireSphere(transform.position, distanceRange);
-                    break;
-                case Skill.Bloc:
-                    break;
-                case Skill.MudThrow:
-                    break;
-                case Skill.Vortex:
-                    break;
-                case Skill.Inverse:
-                    break;
-                case Skill.Mentaliste:
-                    break;
-                case Skill.Shield:
-                    break;
-                case Skill.PreciousWater:
-                    break;
-                case Skill.Rooting:
-                    break;
-                case Skill.Silence:
-                    break;
-                case Skill.Magnet:
-                    break;
-                case Skill.None:
-                    break;
-                default:
-                    break;
-            }
-        }
-        
-
-    }
-
+    #endregion
 }
