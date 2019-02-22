371e7d7f2fd049e2b0864abc28ef064b36956ebe
valentinmoulard <moulardvalentin@gmail.com>
2019-02-22 16:49:16 +0100

    oui

--- Assets/Scripts/Object/Capstan.cs
+++ Assets/Scripts/Object/Capstan.cs
@@ -9,11 +9,24 @@ public class Capstan : MonoBehaviour
     Transform pivot;
     private bool orbOnTheRight;
 
+    int rotationSpeed = 1;
+
+    [Tooltip("how many times the capstan must be turned clockwise to lower the bridge")]
+    public int notchNumber;
+    public int actualAngle;
+    public int targetAngle;
+    public int maxAngle;
+
     private void Start()
     {
         orb = GameManager.gameManager.orb;
         border = transform.GetChild(1);
         pivot = transform.GetChild(2);
+
+        actualAngle = 0;
+        targetAngle = 0;
+
+        maxAngle = 180 * notchNumber;
     }
 
     void Update()
@@ -28,31 +41,50 @@ public class Capstan : MonoBehaviour
             transform.GetChild(0).GetComponent<Renderer>().material.color = Color.blue;
             orbOnTheRight = false;
         }
+
+        if(actualAngle != targetAngle)
+        {
+            int sign = targetAngle > actualAngle ? 1 : -1;
+            transform.localEulerAngles = new Vector3(0.0f, actualAngle += (rotationSpeed * sign), 0.0f);
+        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (orbOnTheRight == false)
+        if (other.CompareTag("Orb"))
         {
-            gameObject.transform.Rotate(new Vector3(0, 180, 0));
-        }
-        else
-        {
-            gameObject.transform.Rotate(new Vector3(0, -180, 0));
+            if (orbOnTheRight == false && targetAngle < maxAngle)
+            {
+                targetAngle += 180;
+            }
+            else if (orbOnTheRight == true && targetAngle > 0)
+            {
+                targetAngle -= 180;
+            }
         }
     }
 
-    bool OnTheRight (Transform pivot, Transform border, Transform otherPos)
+    /// <summary>
+    /// function that return a bool
+    /// check if the object (otherPos) is on the right or left compared to the vector (pivot,border)
+    /// </summary>
+    /// <param name="pivot"></param>
+    /// <param name="border"></param>
+    /// <param name="otherPos"></param>
+    /// <returns></returns>
+    bool OnTheRight(Transform pivot, Transform border, Transform otherPos)
     {
         Vector3 ab = border.position - pivot.position;
         Vector3 ac = otherPos.position - pivot.position;
         Vector3 crossProduct = Vector3.Cross(ab, ac);
-        
+
         if (crossProduct.y < 0)
         {
             return false;
         }
         return true;
     }
+    
 
 }
---
+++ Assets/Scripts/Object/LiftBridge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiftBridge : MonoBehaviour
+{
+    public GameObject capstan;
+    Quaternion startRotation;
+    Quaternion endRotation;
+
+    private void Start()
+    {
+        startRotation = transform.rotation;
+        endRotation = Quaternion.Euler(0, 0, 50);
+    }
+
+    private void Update()
+    {
+        Capstan cap = capstan.GetComponent<Capstan>();
+        transform.rotation = Quaternion.Lerp(startRotation, endRotation, (float)cap.targetAngle / (float)cap.maxAngle);
+    }
+
+}
