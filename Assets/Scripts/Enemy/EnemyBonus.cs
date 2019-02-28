3fbf1370c21a0b528ad0412c93dae06d039528c5
Nathan Desages <nathan.desages@gmail.com>
2019-02-28 11:35:58 +0100

    add mirror bonus for enemies

---
+++ Assets/Scripts/Enemy/EnemyBonus.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBonus : MonoBehaviour
+{
+	public enum Bonus
+	{
+		Mirror,
+		ElementalShield,
+		None
+	}
+
+	public Bonus bonus;
+
+	//Mirror
+	[DrawIf(new string[] { "bonus" }, Bonus.Mirror)]
+	public GameObject mirrorPrefab;
+	GameObject mirrorChild;
+	[DrawIf(new string[] { "bonus" }, Bonus.Mirror)]
+	[Tooltip("represent the mirror's deactivation duration when the enemy is hit")]
+	public float deactivationDuration;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		if (bonus == Bonus.Mirror)
+			mirrorChild = Instantiate(mirrorPrefab, transform);
+    }
+
+	IEnumerator DeactivateShieldCoroutine()
+	{
+		mirrorChild.GetComponent<BoxCollider>().enabled = false;
+		yield return new WaitForSeconds(deactivationDuration);
+		mirrorChild.GetComponent<BoxCollider>().enabled = true;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Orb") && bonus == Bonus.Mirror)
+		{
+			StopCoroutine(DeactivateShieldCoroutine());
+			StartCoroutine(DeactivateShieldCoroutine());
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy/Mirror.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mirror : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Orb"))
+		{
+			OrbController controller = GameManager.gameManager.orb.GetComponent<OrbController>();
+			controller.toPlayer2 = !controller.toPlayer2;
+			GetComponentInParent<NavMeshAgent>().velocity = -GetComponentInParent<NavMeshAgent>().velocity*20;
+		}
+	}
+}
