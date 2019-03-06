d58c02bf06750118ed4ba4a301765a8612570f5e
CairodJD <37753956+CairodJD@users.noreply.github.com>
2019-03-06 14:18:08 +0100

    Update UIManager.cs

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -322,7 +322,7 @@ public class UIManager : MonoBehaviour
         }
         else if (nb >= 2)
         {
-            combo.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Combo !";
+            combo.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Combo !";
             combo.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "x" + nb;
         }
 
