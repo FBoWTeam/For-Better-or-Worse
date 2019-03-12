9f3eea3b32aa32ba7df453bae272dfe09cd0b561
Nathan Desages <nathan.desages@gmail.com>
2019-03-12 10:39:13 +0100

    checkpoint wip

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+	GameObject contentToLoad;
+	GameObject actualContent;
+
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		contentToLoad = transform.GetChild(1).gameObject;
+		actualContent = Instantiate(contentToLoad, transform);
+		Destroy(contentToLoad);
+
+		actualContent = Instantiate(contentToLoad, transform);
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+			Debug.Log("oui");
+	}
+}
