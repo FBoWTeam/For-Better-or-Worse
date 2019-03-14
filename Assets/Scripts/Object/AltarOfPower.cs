55437599bdaf8d8f1cd3562ad389e3aadb97d045
valentinmoulard <moulardvalentin@gmail.com>
2019-03-14 12:01:54 +0100

    oui

---
+++ Assets/Scripts/Object/AltarOfPower.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltarOfPower : MonoBehaviour, IActivable
+{
+    public bool isActive { get; set; }
+
+    
+
+    public void Activate()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Orb"))
+        {
+            GivePower();
+        }
+    }
+
+
+    void GivePower()
+    {
+
+    }
+
+}
