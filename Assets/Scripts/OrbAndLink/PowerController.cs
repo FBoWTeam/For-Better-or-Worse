013b9fe2a653d77551f560b4409144357b7d548f
CairodJD <37753956+CairodJD@users.noreply.github.com>
2019-03-15 11:41:45 +0100

    Ice Power Implementation

--- Assets/Scripts/OrbAndLink/PowerController.cs
+++ Assets/Scripts/OrbAndLink/PowerController.cs
@@ -449,6 +449,12 @@ public class PowerController : MonoBehaviour
         GetComponent<MeshRenderer>().material = normalMaterial;
     }
 
+    IEnumerator Freeze(Enemy target,float duration) {
+        target.isFrozen = true; 
+        yield return new WaitForSeconds(duration);
+        target.isFrozen = false;
+    }
+
     #endregion
 
     #region Fire
@@ -658,6 +664,7 @@ public class PowerController : MonoBehaviour
         {
             case GameManager.PowerType.Ice:
                 Debug.Log("Slow down bitch");
+                StartCoroutine(Freeze(enemy, iceDuration));
                 damageTaken += iceDamage;
                 break;
             case GameManager.PowerType.Fire:
