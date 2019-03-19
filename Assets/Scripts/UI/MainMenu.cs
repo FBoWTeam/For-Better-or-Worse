7d62fe1a7628e5d5ccbc12da9882c83f9fe825f6
Bibu219 <bastien.legault@gmail.com>
2019-03-19 17:02:36 +0100

    UI

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{ 
+    Text percentageText;
+
+    private void Start()
+    {
+        percentageText = GetComponent<Text>();
+    }
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        print("QUIT");
+        Application.Quit();
+    }
+
+
+    public void UpdateText(float value)
+    {
+        percentageText.text = Mathf.RoundToInt(100 * value) + "%";
+    }
+
+
+
+}
---
+++ Assets/Scripts/UI/ShowValue.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowValue : MonoBehaviour
+{
+    Text percentageText;
+
+    private void Start()
+    {
+        percentageText = GetComponent<Text>();
+    }
+
+    public void UpdateText(float value)
+    {
+        percentageText.text = Mathf.RoundToInt(100 * value) + "%";
+    }
+}
