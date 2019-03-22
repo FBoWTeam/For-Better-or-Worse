4fc2d2342bfb5cb97a46c1266b70cd530906bfe6
valentinmoulard <moulardvalentin@gmail.com>
2019-03-22 16:52:01 +0100

    interactable wall maggle

---
+++ Assets/Scripts/Object/InteractableWalls.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableWalls : MonoBehaviour, IActivable
+{
+    public bool isActive { get; set; }
+
+    public bool isDown;
+
+    [Tooltip("list of activated objects needed to open the door")]
+    public List<GameObject> objectsConditions;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInParent<Animator>();
+        if (isDown)
+        {
+            anim.SetBool("isDown", true);
+            isActive = true;
+            isDown = true;
+        }
+    }
+
+    public void Activate()
+    {
+        if (CheckValidObjects())
+        {
+            if (isActive == false)
+            {
+                anim.SetBool("isDown", true);
+                isActive = true;
+                isDown = true;
+            }
+            else if (isActive == true)
+            {
+                anim.SetBool("isDown", false);
+                isActive = false;
+                isDown = false;
+            }
+        }
+    }
+
+
+    bool CheckValidObjects()
+    {
+        for (int i = 0; i < objectsConditions.Count; i++)
+        {
+            if (objectsConditions[i].GetComponent<IActivable>().isActive != true)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
