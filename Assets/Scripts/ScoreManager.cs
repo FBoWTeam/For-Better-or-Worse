8c8a1bc1e10a61ce79ab90a643a134e63b8feaa1
valentinmoulard <moulardvalentin@gmail.com>
2019-03-26 16:30:53 +0100

    uio

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -36,7 +36,7 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(StatPrintCoroutine(timeToWait));
+        //StartCoroutine(StatPrintCoroutine(timeToWait));
     }
 
     // Update is called once per frame
