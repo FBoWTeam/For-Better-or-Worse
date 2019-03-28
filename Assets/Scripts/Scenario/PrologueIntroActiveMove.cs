4084d6fdc3decc2502b7d251f73aa22ff5d0dc28
Nathan Desages <nathan.desages@gmail.com>
2019-03-28 17:16:46 +0100

    add move tutorial and tutorial system

--- Assets/Scripts/Scenario/PrologueIntroActiveMove.cs
+++ Assets/Scripts/Scenario/PrologueIntroActiveMove.cs
@@ -1,13 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
-public class PrologueIntroActiveMove : MonoBehaviour
+public class PrologueIntroActiveMove : Tutorial
 {
+	PlayableDirector director;
+	bool player1HasMoved, player2HasMoved;
+
 	private void OnEnable()
 	{
+		director = GameObject.Find("IntroScenario").GetComponent<PlayableDirector>();
+		StartCoroutine(ActivateMove());
+	}
+
+	public IEnumerator ActivateMove()
+	{
+		director.Pause();
+		GetsIn();
+
 		GameManager.gameManager.player1.GetComponent<PlayerController>().active = true;
 		GameManager.gameManager.player2.GetComponent<PlayerController>().active = true;
-		Destroy(this.gameObject);
+		player1HasMoved = false;
+		player2HasMoved = false;
+
+		while (!(player1HasMoved && player2HasMoved))
+		{
+			if (GameManager.gameManager.player1.GetComponent<PlayerController>().direction != Vector3.zero)
+			{
+				player1HasMoved = true;
+			}
+			if (GameManager.gameManager.player2.GetComponent<PlayerController>().direction != Vector3.zero)
+			{
+				player2HasMoved = true;
+			}
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		StartCoroutine(GetsOut());
+		yield return new WaitForSeconds(3.0f);
+
+		director.Resume();
+		Destroy(gameObject);
 	}
 }
--- Assets/Scripts/Scenario/Tutorial.cs
+++ Assets/Scripts/Scenario/Tutorial.cs
@@ -6,27 +6,23 @@ using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-	Canvas tutorialCanvas;
-
 	public string tutorialText;
 	public Sprite tutorialIcon;
 
-	public void Awake()
-	{
-		tutorialCanvas = GameObject.Find("Tutorials").GetComponent<Canvas>();
-		GetsIn();
-	}
-
     public void GetsIn()
     {
+		GameObject tutorialCanvas = GameManager.gameManager.tutorials;
+		tutorialCanvas.SetActive(true);
 		tutorialCanvas.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = tutorialText;
 		tutorialCanvas.transform.Find("Image").GetComponent<Image>().sprite = tutorialIcon;
 		tutorialCanvas.GetComponent<Animator>().SetTrigger("TutoIn");
     }
 
-	public void GetsOut()
+	public IEnumerator GetsOut()
 	{
+		GameObject tutorialCanvas = GameManager.gameManager.tutorials;
 		tutorialCanvas.GetComponent<Animator>().SetTrigger("TutoOut");
-		Destroy(gameObject);
+		yield return new WaitForSeconds(tutorialCanvas.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
+		tutorialCanvas.SetActive(false);
 	}
 }
