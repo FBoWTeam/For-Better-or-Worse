49d9172ca72d2b95d586cce45c49682d529ca2cc
Nathan Desages <nathan.desages@gmail.com>
2019-04-04 15:30:28 +0200

    test entity spam

---
+++ Assets/Scripts/Scenario/EntitySpam.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EntitySpam : MonoBehaviour
+{
+	public List<string> sentences;
+	TextMeshProUGUI text;
+	public float writeSpeed;
+
+	private void OnEnable()
+	{
+		text = GetComponentInChildren<TextMeshProUGUI>();
+		StartCoroutine(EntitySpamCoroutine());
+	}
+
+	public IEnumerator EntitySpamCoroutine()
+	{
+		bool pass = true;
+		text.text = "";
+		while (true)
+		{
+			foreach(string s in sentences)
+			{
+				foreach(char c in s)
+				{
+					GetComponent<RectTransform>().localPosition += Vector3.up;
+					if (c == '\\')
+						text.text += '\n';
+					else
+						text.text += c;
+
+					pass = !pass;
+					if(!pass)
+					{
+						yield return new WaitForSeconds(writeSpeed);
+					}
+				}
+			}
+		}
+	}
+}
