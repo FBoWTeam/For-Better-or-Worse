d379d2515a4e5381580abbbf2dbb6cde412a7b93
Bibu219 <bastien.legault@gmail.com>
2019-04-08 16:22:41 +0200

    update timeline

--- Assets/Scripts/Scenario/Timeline End Prologue/TimeLineEndPrologue.cs
+++ Assets/Scripts/Scenario/Timeline End Prologue/TimeLineEndPrologue.cs
@@ -13,11 +13,14 @@ public class TimeLineEndPrologue : MonoBehaviour
 
     public void Initialize()
     {
+        GameManager.gameManager.isPaused = true;
         director = GetComponent<PlayableDirector>();
         GameManager.gameManager.orb.GetComponent<OrbController>().canHitPlayer = false;
         GameManager.gameManager.UIManager.gameObject.SetActive(false);
         GameManager.gameManager.blackBands.SetActive(true);
         GameObject.Find("SceneLoader").SetActive(false);
+        GameManager.gameManager.player1.GetComponent<CapsuleCollider>().isTrigger = true;
+        GameManager.gameManager.player2.GetComponent<CapsuleCollider>().isTrigger = true;
 
         director.Play();
         director.stopped += WhenEnded;
@@ -26,6 +29,9 @@ public class TimeLineEndPrologue : MonoBehaviour
     public void WhenEnded(PlayableDirector obj)
     {
         Debug.Log("End Prologue");
+        GameManager.gameManager.player1.GetComponent<CapsuleCollider>().isTrigger = false;
+        GameManager.gameManager.player2.GetComponent<CapsuleCollider>().isTrigger = false;
+        GameManager.gameManager.isPaused = false;
         //SceneManager.LoadScene("test");
     }
 }
