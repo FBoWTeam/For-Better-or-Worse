0504c758d3b4c85603cd0a7de7b62b45a0f1e351
valentinmoulard <moulardvalentin@gmail.com>
2019-04-08 15:56:30 +0200

    oui

--- Assets/Scripts/Object/Door.cs
+++ Assets/Scripts/Object/Door.cs
@@ -93,19 +93,15 @@ public class Door : MonoBehaviour, IActivable
 
     void ActivateElectricDoor()
     {
-        if (doorCurrentCharge < doorMaxCharge)
+        if (doorCurrentCharge >= doorMaxCharge)
         {
-            doorCurrentCharge += chargingAmount;
-            if (doorCurrentCharge >= doorMaxCharge)
+            doorCurrentCharge = doorMaxCharge;
+            if (isActive == false && CheckValidObjects())
             {
-                doorCurrentCharge = doorMaxCharge;
-                if (isActive == false && CheckValidObjects())
-                {
-                    GetComponentInParent<Animation>().Play("DoorOpen");
-                    isActive = true;
-                }
-
+                GetComponentInParent<Animation>().Play("DoorOpen");
+                isActive = true;
             }
+
         }
     }
 
@@ -114,6 +110,7 @@ public class Door : MonoBehaviour, IActivable
     {
         if (other.CompareTag("Orb") && doorType == DoorType.Electric && other.gameObject.GetComponent<PowerController>().elementalPower == GameManager.PowerType.Electric)
         {
+            doorCurrentCharge += chargingAmount;
             this.Activate();
         }
     }
