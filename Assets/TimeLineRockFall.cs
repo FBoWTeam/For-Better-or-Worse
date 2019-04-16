4bc489c59a781c08bc39f21af2024409a29e5b00
Bibu219 <bastien.legault@gmail.com>
2019-04-16 16:13:28 +0200

    update timeline rock fall

---
+++ Assets/TimeLineRockFall.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class TimeLineRockFall : MonoBehaviour
+{
+    PlayableDirector director;
+    GameObject WallForTimeLine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WallForTimeLine = GameObject.Find("Wall reverse");
+        WallForTimeLine.SetActive(false);
+    }
+
+    public void Initialize()
+    {
+        GameManager.gameManager.isPaused = true;
+        GameManager.gameManager.player1.GetComponent<PlayerController>().active = false;
+        GameManager.gameManager.player2.GetComponent<PlayerController>().active = false;
+        GameManager.gameManager.player1.GetComponent<OrbHitter>().active = false;
+        GameManager.gameManager.player2.GetComponent<OrbHitter>().active = false;
+        director = GetComponent<PlayableDirector>();
+        StartCoroutine(InitCoroutine());
+
+        director.Play();
+        director.stopped += WhenEnded;
+    }
+
+    public void WhenEnded(PlayableDirector obj)
+    {
+        WallForTimeLine.SetActive(false);
+        GameManager.gameManager.player1.GetComponent<CapsuleCollider>().isTrigger = false;
+        GameManager.gameManager.player2.GetComponent<CapsuleCollider>().isTrigger = false;
+        GameManager.gameManager.isPaused = false;
+        SceneManager.LoadScene(3);
+    }
+
+    IEnumerator InitCoroutine()
+    {
+        yield return new WaitForSeconds(1.5f);//temps a changer
+        WallForTimeLine.SetActive(true);
+        GameManager.gameManager.isPaused = false;
+        GameManager.gameManager.orb.GetComponent<OrbController>().canHitPlayer = false;
+        GameManager.gameManager.UIManager.gameObject.SetActive(false);
+        GameManager.gameManager.blackBands.SetActive(true);
+        GameManager.gameManager.player1.GetComponent<CapsuleCollider>().isTrigger = true;
+        GameManager.gameManager.player2.GetComponent<CapsuleCollider>().isTrigger = true;
+    }
+
+}
