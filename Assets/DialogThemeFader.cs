fcf654d3623c462fa35f7d2931ab29cd07ff4c58
Nathan Desages <nathan.desages@gmail.com>
2019-04-23 19:29:21 +0200

    music prologue V2

---
+++ Assets/DialogThemeFader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogThemeFader : MonoBehaviour
+{
+	private void OnEnable()
+	{
+		StartCoroutine(FadeVolume());
+	}
+
+	IEnumerator FadeVolume()
+	{
+		AudioSource theme = GameObject.Find("DialogTheme").GetComponent<AudioSource>();
+		while (theme.volume >= 0.0f)
+		{
+			theme.volume -= 0.01f;
+			yield return new WaitForEndOfFrame();
+		}
+		Destroy(this.gameObject);
+	}
+}
