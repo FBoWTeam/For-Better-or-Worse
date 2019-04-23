8ec795be6603833e0aafbe7df7802cfa7d754768
Nathan Desages <nathan.desages@gmail.com>
2019-04-23 23:26:23 +0200

    various fixes and adding new tuto on timeline

--- Assets/PrologueBGMVolumeController.cs
+++ Assets/PrologueBGMVolumeController.cs
@@ -12,7 +12,7 @@ public class PrologueBGMVolumeController : MonoBehaviour
 	IEnumerator BoostBGMVolume()
 	{
 		AudioSource bgm = GameObject.Find("BGM").GetComponent<AudioSource>();
-		while(bgm.volume <= 1.0f)
+		while(bgm.volume <= 0.4f)
 		{
 			bgm.volume += 0.01f;
 			yield return new WaitForEndOfFrame();
