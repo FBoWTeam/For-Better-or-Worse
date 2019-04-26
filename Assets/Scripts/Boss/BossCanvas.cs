f633f4b483bc1a2de8fcc762c96331f2b4390340
Nathan Desages <nathan.desages@gmail.com>
2019-04-26 12:09:29 +0200

    add boss health bar

---
+++ Assets/Scripts/Boss/BossCanvas.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossCanvas : MonoBehaviour
+{
+	BossSystem boss;
+
+	Image healthRemainingBar;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		healthRemainingBar = transform.GetChild(0).GetChild(0).GetComponent<Image>();
+		boss = GetComponentInParent<BossSystem>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.LookAt(Camera.main.transform.position);
+		healthRemainingBar.fillAmount = (float)boss.hp / (float)boss.baseHP;
+	}
+}
