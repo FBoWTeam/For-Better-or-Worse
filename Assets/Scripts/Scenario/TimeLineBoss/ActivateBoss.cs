ece7e3700e12b79fb62c67dae7bb2c60ca116252
Nathan Desages <nathan.desages@gmail.com>
2019-04-29 05:26:20 +0200

    boss intro timeline skippable + fixes + music on boss + skip credits

--- Assets/Scripts/Scenario/TimeLineBoss/ActivateBoss.cs
+++ Assets/Scripts/Scenario/TimeLineBoss/ActivateBoss.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 
 public class ActivateBoss : MonoBehaviour
 {
+	public RuntimeAnimatorController RAC;
+	public AudioClip bossTheme;
     public GameObject boss;
+	public GameObject protectionPylon;
 
+	public void Awake()
+	{
+		boss.GetComponent<Animator>().runtimeAnimatorController = null;
+	}
 
-    private void OnEnable()
-    {
-        boss.GetComponent<BossSystem>().enabled = true;
-        boss.transform.localScale = new Vector3(1, 1, 1);
+	private void OnDestroy()
+	{
+		AudioSource bgm = GameObject.Find("BGM").GetComponent<AudioSource>();
+		if(bgm.clip != bossTheme)
+		{
+			bgm.clip = bossTheme;
+			bgm.Play();
+			bgm.volume = 0.4f;
+		}
+		boss.GetComponent<Animator>().runtimeAnimatorController = RAC;
+		boss.GetComponent<BossSystem>().enabled = true;
+		boss.transform.position = new Vector3(1.5f, 0.0f, -20.5f);
+		boss.transform.localScale = new Vector3(1, 1, 1);
+		protectionPylon.transform.position = new Vector3(1.5f, 0.0f, -20.5f);
     }
 }
