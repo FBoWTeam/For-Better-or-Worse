5c22a33247832d3139f57002b96b2bc5403b4242
Bibu219 <bastien.legault@gmail.com>
2019-04-29 11:09:33 +0200

    timeline death

--- Assets/Scripts/Scenario/TimeLineBoss/TimeLineDeath.cs
+++ Assets/Scripts/Scenario/TimeLineBoss/TimeLineDeath.cs
@@ -8,21 +8,32 @@ using UnityEngine.SceneManagement;
 public class TimeLineDeath : MonoBehaviour
 {
     PlayableDirector director;
+    GameObject Boss;
+    public GameObject deathPoof;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        Boss = GameObject.Find("Boss");
     }
 
     public void Initialize()
     {
         GameManager.gameManager.orb.GetComponent<OrbController>().canHitPlayer = false;
 
+        StartCoroutine(DeathPoofBoss());
+
         director = GetComponent<PlayableDirector>();
         director.Play();
         director.stopped += WhenEnded;
     }
+
+    IEnumerator DeathPoofBoss()
+    {
+        yield return new WaitForSeconds(2.5f);
+        Instantiate(deathPoof, Boss.transform.position, Quaternion.identity);
+    }
+
 
     public void WhenEnded(PlayableDirector obj)
     {
