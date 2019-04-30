a5b2cf724a9062888c3a87d4ca3ca616a54e6950
Bibu219 <bastien.legault@gmail.com>
2019-04-30 11:05:24 +0200

    fix death boss

--- Assets/Scripts/Scenario/TimeLineBoss/TimeLineDeath.cs
+++ Assets/Scripts/Scenario/TimeLineBoss/TimeLineDeath.cs
@@ -2,23 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 
 public class TimeLineDeath : MonoBehaviour
 {
     PlayableDirector director;
+    GameObject Boss;
+    public GameObject deathPoof;
     GameObject bossHealthBar;
 
     // Start is called before the first frame update
     void Start()
     {
+        Boss = GameObject.Find("Boss");
         bossHealthBar = GameObject.Find("Bosshealthbar");
     }
 
     public void Initialize()
     {
-        director.Play();
+        GameManager.gameManager.orb.GetComponent<OrbController>().canHitPlayer = false;
+
+        StartCoroutine(DeathPoofBoss());
         bossHealthBar.SetActive(false);
+
+        director = GetComponent<PlayableDirector>();
+        director.Play();
+        director.stopped += WhenEnded;
     }
 
+    IEnumerator DeathPoofBoss()
+    {
+        yield return new WaitForSeconds(3.5f);
+        Instantiate(deathPoof, Boss.transform.position, Quaternion.identity);
+    }
+
+
+    public void WhenEnded(PlayableDirector obj)
+    {
+        GameData.previousScene = 9;
+        SceneManager.LoadScene(10);
+    }
 }
