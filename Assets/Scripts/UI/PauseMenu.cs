f2587d0461b635d877e94f2e026c17c802943f72
Piteul <axel.tetart@gmail.com>
2019-04-30 14:24:21 +0200

    Revert "Merge branch 'Pit-LastBranchOfMyLife' into Develop"
    This reverts commit baeef1a71dd49fd9aeed928d2d8f08a54ca977fd, reversing
    changes made to 272fe315fdad41432cda12c8241f464723796800.

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -2,51 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
 
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool gameIsPaused = false;
 
-    [Header("Main Components")]
-    public GameObject mainMenu;
-    public GameObject guide;
-
-    [Header("Main Components")]
-    public GameObject mappingPanel;
-    public GameObject powerPanel;
-
-    [Header("Difficulty")]
-    public TextMeshProUGUI difficultyText;
-    public TextMeshProUGUI difficultyExplicationText;
-
-    [Header("Mapping")]
-    public Image mappingImage;
-    public Sprite mappingVF;
-    public Sprite mappingVA;
-
-    private void Awake()
-    {
-        if (GameData.english)
-        {
-            mappingImage.sprite = mappingVA;
-        }
-        else
-        {
-            mappingImage.sprite = mappingVF;
-        }
-
-        CheckDifficulty();
-    }
+    public GameObject pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7) || Input.GetKeyDown(KeyCode.Joystick2Button7))
         {
-            if (GameManager.gameManager.isPaused)
+            if(gameIsPaused)
             {
                 Resume();
             }
@@ -61,54 +30,16 @@ public class PauseMenu : MonoBehaviour
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        GameManager.gameManager.isPaused = false;
+        gameIsPaused = false;
     }
 
     void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
-        GameManager.gameManager.isPaused = true;
+        gameIsPaused = true;
     }
 
-    public void OpenGuide()
-    {
-        mainMenu.SetActive(false);
-        guide.SetActive(true);
-    }
-
-    public void ChangeDifficulty()
-    {
-        GameData.worseModeActivated = (GameData.worseModeActivated) ? false : true;
-
-        CheckDifficulty();
-    }
-
-    public void CheckDifficulty()
-    {
-        if (GameData.worseModeActivated)
-        {
-            difficultyText.SetText(I18n.Translate("menu.pause.bouton6"));
-            difficultyExplicationText.SetText(I18n.Translate("menu.niveaux.difficulte5"));
-        }
-        else
-        {
-            difficultyText.SetText(I18n.Translate("menu.pause.bouton5"));
-            difficultyExplicationText.SetText(I18n.Translate("menu.niveaux.difficulte3"));
-        }
-    }
-
-    public void OpenMappingPanel()
-    {
-        powerPanel.SetActive(false);
-        mappingPanel.SetActive(true);
-    }
-
-    public void OpenPowerPanel()
-    {
-        mappingPanel.SetActive(false);
-        powerPanel.SetActive(true);
-    }
     public void LoadMenu()
     {
         Debug.Log("Menu");
