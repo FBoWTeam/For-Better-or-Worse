a00b62c677e3adf0552f66801ce9e7363c1482dd
Nathan Desages <nathan.desages@gmail.com>
2019-05-01 14:39:24 +0200

    add mouse locker

---
+++ Assets/MouseLocker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseLocker : MonoBehaviour
+{
+	GameObject lastselect;
+
+	private void Awake()
+	{
+		Cursor.visible = false;
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Start()
+	{
+		lastselect = null;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (EventSystem.current.currentSelectedGameObject == null)
+		{
+			EventSystem.current.SetSelectedGameObject(lastselect);
+		}
+		else
+		{
+			lastselect = EventSystem.current.currentSelectedGameObject;
+		}
+	}
+}
